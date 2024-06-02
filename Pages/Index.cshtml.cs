602314aff7bb42106892a8773c1b8d29f496cab6
Ganeshkumar <rgkjobmail@gmail.com>
2024-06-02 16:04:36 +0530

    update configuration

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace azure_app_trev.Pages;
 
@@ -7,13 +6,16 @@ public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration _configuration)
     {
         _logger = logger;
+        Configuration = _configuration;
     }
+
+    public IConfiguration Configuration { get; }
 
     public void OnGet()
     {
-
+        ViewData["Greeting"] = Configuration["Greeting"];
     }
 }
